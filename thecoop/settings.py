39e09ed8e6ad71cb761402adba24dd76dccdef4f
varundrogba <varundrogba@gmail.com>
2016-03-01 14:48:41 +0530

    csrf token error related changes

--- thecoop/settings.py
+++ thecoop/settings.py
@@ -44,9 +44,9 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
